6d8d3d9b45991cfc89533768d49be5f94ac6a178
Sean Bailey <sean@seanbailey.io>
2017-06-06 00:45:46 +1000

    Initial commit.

---
+++ README.md
@@ -0,0 +1,2 @@
+# Atomic Core
+The core plugin for the Atomic Hive Minecraft server.
---
+++ src/net/atomichive/core/Main.java
@@ -0,0 +1,31 @@
+package net.atomichive.core;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Main
+ * Main class for the atomic-core plugin.
+ */
+public class Main extends JavaPlugin {
+
+
+    /**
+     * On enable
+     * Run whenever the server enables this plugin.
+     */
+    @Override
+    public void onEnable() {
+
+    }
+
+
+    /**
+     * On disable
+     * Run whenever the server disables this plugin.
+     */
+    @Override
+    public void onDisable() {
+
+    }
+
+}
