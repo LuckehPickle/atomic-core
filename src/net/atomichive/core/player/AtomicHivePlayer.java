0cea3a7b40c7fa80967bb1a519b100dc78a75c9f
Quinster <q.r.depreter@gmail.com>
2017-06-09 17:02:20 +0200

    Moved principe to faction plugin

--- src/net/atomichive/core/player/AtomicHivePlayer.java
+++ src/net/atomichive/core/player/AtomicHivePlayer.java
@@ -2,19 +2,8 @@ package net.atomichive.core.player;
 
 public class AtomicHivePlayer {
 	
-	private int level;
-	private double experience;
-	
 	public AtomicHivePlayer() {
 		
-	}
-	
-	public int getLevel() {
-		return level;
-	}
-	
-	public double getExperience() {
-		return experience;
 	}
 
 }
